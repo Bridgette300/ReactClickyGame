0a352dca9a1c568eb17e5ce3e71b4da05b1bdbed
Bridgette Smith <bsmith@ift.org>
2018-07-02 20:10:09 -0500

    add components

---
+++ app/src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => (
+   <header className="header">
+   	<h1>Click Game!</h1>
+   	<h2>Click on ONE image to earn points!</h2>
+   </header>
+);
+export default Header;
